7073a63b802cd6fa8cb8b1ac06079fd5b2309465
Vinicius Coelho <vinicius_coelho@live.com>
2021-03-08 22:02:11 -0400

    :wrench: chore: add dotenv to typeorm config file

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [
