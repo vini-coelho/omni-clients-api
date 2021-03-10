6479cec3dbda9b86b1309cc5721177e15c3a49c2
Vinicius Coelho <vinicius_coelho@live.com>
2021-03-10 15:59:19 -0400

    feat: add jwt auth and password encryption

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,7 @@
 import { getRepository } from "typeorm";
 import { NextFunction, Request, Response } from "express";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import { User } from "../models/User";
 
 
@@ -11,18 +13,44 @@ export async function all(request: Request, response: Response, next: NextFuncti
   return response.json(users);
 }
 
-export async function one(request: Request, response: Response, next: NextFunction) {
-  const userRepository = getRepository(User);
-
-  return userRepository.findOne(request.params.id);
-}
-
 export async function save(request: Request, response: Response, next: NextFunction) {
-  const userRepository = getRepository(User);
+  const userRespository = getRepository(User);
 
-  const user = await userRepository.save(request.body);
+  const userExists = await userRespository.findOne({
+    where: [
+      {
+        email: request.body.email,
+      }
+    ],
+  });
 
-  return response.json(user);
+  if (!!userExists) {
+    return response.status(400).json({ message: 'User already exists.' });
+  }
+
+  try {
+
+    const hash = await bcrypt.hash(request.body.password, 10);
+
+    const newUser = { ...request.body, password: hash };
+
+    let user = new User()
+
+    user = newUser;
+
+    // const user = userRespository.create(newUser);
+
+    const result = await userRespository.save(user);
+
+    const token = jwt.sign(String(result.id), process.env.APP_SECRET);
+
+    result.password = undefined;
+
+    return response.json({ user: result, token });
+  } catch (err) {
+    return response.status(400).json({ error: err.message })
+  }
+
 }
 
 export async function remove(request: Request, response: Response, next: NextFunction) {
