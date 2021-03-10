9d5e3daf63a966ed46ad0e90f55d16591f3a67bf
Vinicius Coelho <vinicius_coelho@live.com>
2021-03-10 19:05:37 -0400

    :hammer: change: switches from pg to sqlite db

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,11 @@
 module.exports = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: 'sqlite',
+  database: './src/database/database.sqlite',
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
   synchronize: true,
   logging: false,
   entities: [
